30eda2ca1cddcda817678a6f53db305a66ca201e
kevin2b <25062561+kevin2b@users.noreply.github.com>
2026-02-03 15:47:50 -0500

    Add filtering and ordering for browsing products

--- src/pages/Product/Product.jsx
+++ src/pages/Product/Product.jsx
@@ -1,22 +1,39 @@
-import { useParams } from "react-router";
+import { useParams, useNavigate, useLocation } from "react-router";
 import { useSelector } from "react-redux";
 
 function Product(){
+  const navigate = useNavigate();
+  const location = useLocation();
   const {id} = useParams();
   const products = useSelector((state) => state.products);
   const product = products.find(product => product.id === Number(id));
 
+  function handleBack() {
+    //If there is a back history, key should not be default
+    if (location.key === 'default') {
+      navigate("/shop");
+    } else {
+      navigate(-1); 
+    }
+  }
+
   if (!product){
-    return <section> Product does not exist! </section>
+    return (
+    <section>
+      <button type="button" onClick={handleBack}> Back </button>
+      Product does not exist! 
+    </section>
+    )
   }
 
   return (
     <section>
+      <button type="button" onClick={handleBack}> Back </button>
       <img src={product.image} alt="Product image" width="100px"/>
       <div> {product.title} </div>
       <div> {product.description} </div>
       <div> Price: ${product.price} </div>
-      <div> Stock: {product.stock} </div>
+      <div> {product.stock > 0 ? "Stock: " + product.stock : "Out of stock!" } </div>
     </section>
   )
 }
--- src/pages/Shop/Shop.jsx
+++ src/pages/Shop/Shop.jsx
@@ -1,17 +1,72 @@
 import { useSelector } from "react-redux";
 import ShopProduct from "./ShopProduct";
+import { useSearchParams } from "react-router";
 
 function Shop(){
-  const products = useSelector((state) => state.products);
+  const allProducts = useSelector((state) => state.products);
+  const [searchParams, setSearchParams] = useSearchParams();
+  const term = searchParams.get("term") || "";
+  const category = searchParams.get("category") || "all"; 
+  const order = searchParams.get("order") || "nameAsc";
+  let products = filterProducts(allProducts, term, category);
+  products = orderProducts(products, order);
+
   return (
     <section> 
-      This is shop page
+      <input type="text" name="search" id="search" placeholder="Enter product name" value={term} onChange={(e) => setSearchParams({ term: e.target.value, category, order})}/>
+      <label htmlFor="category"> Sort By: </label>
+      <select id="category" name="category" value={category} onChange={(e) => setSearchParams({term, category: e.target.value, order})}>
+        <option value="all"> All </option>
+        <option value="men's clothing"> Men's Clothing </option>
+        <option value="women's clothing"> Women's Clothing </option>
+        <option value="jewelery"> Jewelery </option>
+        <option value="electronics"> Electronics </option>
+      </select>
+      <label htmlFor="order"> Sort By: </label>
+      <select id="order" name="order" value={order} onChange={(e) => setSearchParams({term, category, order: e.target.value})}>
+        <option value="nameAsc"> Name: A to Z </option>
+        <option value="nameDesc"> Name: Z to A </option>
+        <option value="priceAsc"> Price: Low to High </option>
+        <option value="priceDesc"> Price: High to Low </option>
+      </select>
       { products.map(product => (
-          <ShopProduct key={product.id} id={product.id} image={product.image} title={product.title} price={product.price} stock= {product.stock}/>
+          <ShopProduct key={product.id} {...product}/>
         ))
       }
+      {products.length === 0 && <div> No products found!</div>}
     </section>
   )
+}
+
+function filterProducts(allProducts, term, category){
+  let products = allProducts;
+  //Prevent no stock items from being shown
+  products = products.filter(product => product.stock > 0);
+
+  //Filter by term
+  if (term.trimStart() !== ""){
+    products = products.filter(product => product.title.toLowerCase().includes(term.trimStart().toLowerCase()));
+  }
+
+  //Filter by category
+  if (category !== "all"){
+    products = products.filter(product => product.category === category);
+  }
+  return products;
+}
+
+function orderProducts(products, order){
+  let productsDup = [...products];
+  switch(order){
+    case "nameDesc":
+      return productsDup.sort((a,b) => b.title.localeCompare(a.title));
+    case "priceAsc":
+      return productsDup.sort((a,b) => a.price - b.price);
+    case "priceDesc":
+      return productsDup.sort((a,b) => b.price - a.price);
+    default:
+      return productsDup.sort((a,b) => a.title.localeCompare(b.title));
+  }
 }
 
 export default Shop;
