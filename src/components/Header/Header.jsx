51e02326a0b95f87bf8d01f1293d5b870b493bfc
kevin2b <25062561+kevin2b@users.noreply.github.com>
2026-02-12 14:15:51 -0500

    Styled header for mobile

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,21 +1,24 @@
 import { Link } from "react-router";
 import styles from "./Header.module.css";
+import logo from '@/assets/images/logo.svg';
+import cart from '@/assets/images/cart.svg';
 
 function Header(){
   return (
     <header>
-      <div>
-        <Link to="/" className={styles.storeName}> Lotus </Link>
+      <div className={styles.firstRow}>
+        <Link to="/" className={styles.storeLogoName}> 
+          <img src={logo} width="50px" height="auto" alt="Aura Logo" className={styles.logo}/>
+          <span className={styles.storeName}> Aura </span>
+        </Link>
+        <Link to="/cart" className={styles.cart}> 
+          <img src={cart} width="25px" height="auto" alt="Aura Logo" className={styles.cartImg}/>
+        </Link>
       </div>
-      <nav>
-      <Link to="/"> Home</Link>
-      {` | `}
-      <Link to="/shop"> Shop </Link>
-      {` | `}
-      <Link to="/about"> About </Link>
-      {` | `}
-      <Link to="/cart"> Cart </Link>
-    </nav>
+      <nav className={styles.nav}>
+        <Link to="/shop" className={styles.linkButton}> Shop </Link>
+        <Link to="/about" className={styles.linkButton}> About </Link>
+      </nav>
     </header>
   )
 }
